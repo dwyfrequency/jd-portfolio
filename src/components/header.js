8623fdc0d75413a92596479a6c35502125bc8025
dwyfrequency <jdwy215@gmail.com>
2019-05-29 21:32:58 -0400

    removing bootstrap

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
 
 const Header = () => {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Link to="/" className="nav-link">
-        <Navbar.Brand>JACK DWYER</Navbar.Brand>
-      </Link>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <li className="nav-item active">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-            <span className="sr-only">(current)</span>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/blog" className="nav-link">
-              Blog
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="contact" className="nav-link">
-              Contact
-            </Link>
-          </li>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  )
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link to="/" className="navbar-brand">
