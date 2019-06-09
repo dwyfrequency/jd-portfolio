fa7778b56ac92d8cda47fd4dc1905651ca868e8d
dwyfrequency <jdwy215@gmail.com>
2019-06-09 10:45:17 -0400

    removing unnecessary imports

--- src/components/portfolioProjects/Projects.js
+++ src/components/portfolioProjects/Projects.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Project from "./Project"
-import styled from "styled-components"
 import styles from "../../css/items.module.css"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -27,15 +26,6 @@ const query = graphql`
   }
 `
 
-// const ProjectsContainer = styled.div`
-//   width: 80vw;
-//   margin: 3rem auto;
-//   display: grid;
-//   grid-template-columns: repeat(auto-fill, minmax(375px, 1fr));
-//   grid-column-gap: 2rem;
-//   grid-row-gap: 2rem;
-// `
-
 const Projects = () => {
   const { projects } = useStaticQuery(query)
   console.log(projects)
@@ -47,17 +37,6 @@ const Projects = () => {
           <Project key={node.id} projectDetails={node} />
         ))}
       </div>
-    </section>
-  )
-  return (
-    <section>
-      <h1>Portfolio</h1>
-
-      {/* <ProjectsContainer> */}
-      {projects.edges.map(({ node }) => (
-        <Project key={node.id} projectDetails={node} />
-      ))}
-      {/* </ProjectsContainer> */}
     </section>
   )
 }
