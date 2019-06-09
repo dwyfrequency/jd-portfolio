65fa006ad7199d3128cdf613551fcfb3ea275676
dwyfrequency <jdwy215@gmail.com>
2019-06-09 11:01:57 -0400

    further troubleshooting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,20 @@
 import React from "react"
 import Layout from "../components/Layout"
-import StyledHero from "../components/StyledHero"
-import Banner from "../components/Banner"
+// import StyledHero from "../components/StyledHero"
+// import Banner from "../components/Banner"
 import { graphql } from "gatsby"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
+// import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 export default ({ data }) => (
   <Layout>
-    <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
+    hello from index
+    {/* <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
       <Banner title="building solutions" info="Developer | Designer | Speaker">
         <AniLink fade to="/portfolio" className="btn-white">
           explore projects
         </AniLink>
       </Banner>
-    </StyledHero>
+    </StyledHero> */}
   </Layout>
 )
 
