e65ea4762803bf1daf16728e646b84fadc035710
dwyfrequency <jdwy215@gmail.com>
2019-06-09 11:48:23 -0400

    adding back Layout naming and 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/Lay"
 import styles from "../css/error.module.css"
 import Banner from "../components/Banner"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
