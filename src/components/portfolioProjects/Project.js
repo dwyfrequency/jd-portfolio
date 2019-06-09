4a4b578be9b4a38c28fa582476dd094df481f300
dwyfrequency <jdwy215@gmail.com>
2019-06-08 23:09:15 -0400

    updated css and component structure - major key was changing image size to be same on mac

--- src/components/portfolioProjects/Project.js
+++ src/components/portfolioProjects/Project.js
@@ -1,6 +1,8 @@
 import React from "react"
 import styled from "styled-components"
 import Image from "gatsby-image"
+import styles from "../../css/project.module.css"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const ProjectCard = styled.div`
   border: 2px solid green;
@@ -27,6 +29,37 @@ const ProjectCard = styled.div`
 `
 
 const Project = ({ projectDetails }) => {
+  return (
+    <article className={styles.project}>
+      <div className={styles.imgContainer}>
+        <Image
+          fluid={projectDetails.projectImg.fluid}
+          className={styles.img}
+          alt="single project"
+        />
+        <a
+          fade
+          className={styles.link}
+          href={projectDetails.projectLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Site
+        </a>
+      </div>
+      <hr style={{ border: "2px solid var(--darkGrey)" }} />
+      <div className={styles.footer}>
+        <h3>{projectDetails.title}</h3>
+        <div className={styles.info}>
+          <h4 className={styles.country}>"default country"</h4>
+          <div className={styles.details}>
+            <h6>12 days</h6>
+            <h6>from $100</h6>
+          </div>
+        </div>
+      </div>
+    </article>
+  )
   return (
     <ProjectCard>
       <div className="imageContainer">
--- src/components/portfolioProjects/Projects.js
+++ src/components/portfolioProjects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Project from "./Project"
 import styled from "styled-components"
+import styles from "../../css/items.module.css"
 import { useStaticQuery, graphql } from "gatsby"
 
 const query = graphql`
@@ -26,26 +27,37 @@ const query = graphql`
   }
 `
 
-const ProjectsContainer = styled.div`
-  width: 80vw;
-  margin: 3rem auto;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(375px, 1fr));
-  grid-column-gap: 2rem;
-  grid-row-gap: 2rem;
-`
+// const ProjectsContainer = styled.div`
+//   width: 80vw;
+//   margin: 3rem auto;
+//   display: grid;
+//   grid-template-columns: repeat(auto-fill, minmax(375px, 1fr));
+//   grid-column-gap: 2rem;
+//   grid-row-gap: 2rem;
+// `
 
 const Projects = () => {
   const { projects } = useStaticQuery(query)
   console.log(projects)
   return (
-    <section>
-      <h1>Portfolio</h1>
-      <ProjectsContainer>
+    <section className={styles.tours}>
+      {/* <Title title="our" subtitle="tours" /> */}
+      <div className={styles.center}>
         {projects.edges.map(({ node }) => (
           <Project key={node.id} projectDetails={node} />
         ))}
-      </ProjectsContainer>
+      </div>
+    </section>
+  )
+  return (
+    <section>
+      <h1>Portfolio</h1>
+
+      {/* <ProjectsContainer> */}
+      {projects.edges.map(({ node }) => (
+        <Project key={node.id} projectDetails={node} />
+      ))}
+      {/* </ProjectsContainer> */}
     </section>
   )
 }
