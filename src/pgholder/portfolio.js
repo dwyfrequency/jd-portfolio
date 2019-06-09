8d67cc0d13f8356374594eead6932c11e6a83d2d
dwyfrequency <jdwy215@gmail.com>
2019-06-09 11:38:55 -0400

    changing name

--- src/pgholder/portfolio.js
+++ src/pgholder/portfolio.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/Lay"
 import Projects from "../components/portfolioProjects/Projects"
 
 const Portfolio = props => {
