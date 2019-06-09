7f5ea95041bf7373c89bc6d09161134abc96ea01
dwyfrequency <jdwy215@gmail.com>
2019-06-09 09:24:35 -0400

    removing styled components where unused

--- src/components/portfolioProjects/Project.js
+++ src/components/portfolioProjects/Project.js
@@ -1,32 +1,7 @@
 import React from "react"
-import styled from "styled-components"
 import Image from "gatsby-image"
 import styles from "../../css/project.module.css"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
-
-const ProjectCard = styled.div`
-  border: 2px solid green;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: stretch;
-  .detailContainer {
-    height: 200px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    /* :last-child {
-      margin: auto 0;
-    } */
-  }
-  .links {
-    border: 2px solid red;
-    display: flex;
-    justify-content: space-around;
-    align-items: baseline;
-    flex-direction: row;
-  }
-`
 
 const Project = ({ projectDetails }) => {
   return (
@@ -60,33 +35,6 @@ const Project = ({ projectDetails }) => {
         </div>
       </div>
     </article>
-  )
-  return (
-    <ProjectCard>
-      <div className="imageContainer">
-        <Image fluid={projectDetails.projectImg.fluid} />
-      </div>
-      <div className="detailContainer">
-        <h2>{projectDetails.title}</h2>
-        <p>{projectDetails.description.description}</p>
-        <div className="links">
-          <a
-            href={projectDetails.projectLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            demo
-          </a>
-          <a
-            href={projectDetails.githubLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            code
-          </a>
-        </div>
-      </div>
-    </ProjectCard>
   )
 }
 
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,7 +5,6 @@ import Projects from "../components/portfolioProjects/Projects"
 const Portfolio = props => {
   return (
     <Layout>
-      <div>Portfolio will live here</div>
       <Projects />
     </Layout>
   )
