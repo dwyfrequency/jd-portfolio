3a8344f80205e21f57747538884b96bc37e7408e
dwyfrequency <jdwy215@gmail.com>
2019-07-06 10:39:14 -0400

    adding updated skills

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -1,5 +1,13 @@
 export default {
-  core: ["HTML & CSS", "JAVASCRIPT", "NODE.JS", "SQL", "GIT", "Design"],
-  libraries: ["REACT", "REDUX", "GATSBY", "EXPRESS", "SEQUELIZE"],
+  core: [
+    "HTML & CSS",
+    "JAVASCRIPT",
+    "NODE.JS",
+    "SQL",
+    "GIT",
+    "Design",
+    "GraphQL",
+  ],
+  libraries: ["REACT", "REDUX", "GATSBY", "EXPRESS", "SEQUELIZE", "Electron"],
   services: ["Github", "HEROKU", "NETLIFY"],
 }
